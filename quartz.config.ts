913caaa087a61813012596edd46f7bfc3fc9917f
PaulDavidTucker <tuckerpauldavid@gmail.com>
2026-07-26 18:24:03 +0100

    chore(config): set baseUrl, drop templates/** from renders, fix pageTitle

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Home",
+    pageTitle: "Paul Tucker",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,8 +16,8 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    baseUrl: "pauldavidtucker.co.uk",
+    ignorePatterns: ["private", "templates/**", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
